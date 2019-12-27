67961c941e8e004e7f88d48a9d3a82bd1518d1c7
Isabelle <ibernard@necando.com>
2019-12-27 11:17:19 -0500

    other index app example with jinga for loop. added .pyc to gitignore

--- Lecture_2_Flask/flask_application/app.py
+++ Lecture_2_Flask/flask_application/app.py
@@ -1,20 +1,14 @@
 from flask import Flask, render_template
-import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    now = datetime.datetime.now()
-    new_year = now.month == 1 and now.day == 1
-    return render_template('index.html', new_year=new_year)
+    names = ["Alice", "Bob", "Isa"]
+    return render_template("index.html", names=names)
 
 
-@app.route("/bye")
-def bye():
-    headline = "Goodbye!"
-    return render_template("index.html", headline=headline)
 
 
 
