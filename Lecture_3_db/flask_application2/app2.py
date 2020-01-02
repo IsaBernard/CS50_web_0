f637c2b4d0b3fcb362da5235a2ffab88bbcfd61d
Isabelle <ibernard@necando.com>
2020-01-02 11:38:33 -0500

    created a new Flask app using sqlalchemy to connect and extract form database

--- Lecture_3_db/flask_application2/app2.py
+++ Lecture_3_db/flask_application2/app2.py
@@ -2,40 +2,29 @@ import os
 
 from flask import Flask, render_template, request
 from sqlalchemy import create_engine
-from sql
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/")
 def index():
-    if session.get("notes") is None:
-        session["notes"] = []
-    if request.method == "POST":
-        note = request.form.get("note")
-        session["notes"].append(note)
-    return render_template("index2.html", notes=session["notes"])
+    flights = db.execute("SELECT * FROM flights").fetchall()
+    return render_template("index2.html", flights=flights)
 
 
-# @app.route("/hello_2", methods=["POST", "GET"])
-# def hello():
-#     if request.method == "GET":
-#         return "Please submit the form instead"
-#     else:
-#         name = request.form.get("name")
-#         return render_template("hello_2.html", name=name)
-#
-#
-# @app.route("/more")
-# def more():
-#     return render_template("more.html")
+@app.route("/book", methods=["POST"])
+def book():
+    """Book a flight"""
 
-
-
+    # Get form information.
+    name = request.form.get("name")
+    try:
+        flight_id = int(request.form.get("flight_id"))
+    except ValueError:
 
 
 
