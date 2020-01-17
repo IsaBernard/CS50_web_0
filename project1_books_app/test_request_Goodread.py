e90d4f8c1110b919d195fb2d4cf2f9261179e6f9
Isabelle <isabellebernard514@gmail.com>
2020-01-17 13:58:53 -0500

    removed API key to hidden file in .gitignore

--- project1_books_app/test_request_Goodread.py
+++ project1_books_app/test_request_Goodread.py
@@ -1,5 +1,8 @@
 import requests
+import os
+
+API_KEY = os.getenv('API_KEY')
 
 res = requests.get("https://www.goodreads.com/book/review_counts.json",
-             params={"key": "rs0FMt22nijlGe2eBNP9Xg", "isbns": "9781632168146"})
+             params={"key": API_KEY, "isbns": "9781632168146"})
 print(res.json())
