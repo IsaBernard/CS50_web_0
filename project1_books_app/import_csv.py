371f0df096500a6396a7efa4c9bc520cd62fe79e
Isabelle <ibernard@necando.com>
2020-01-24 13:08:47 -0500

    import.py will insert values from csv books into db table books, but we first need to create the table

--- project1_books_app/import_csv.py
+++ project1_books_app/import_csv.py
@@ -7,14 +7,18 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+
 def main():
-    f = open("flights.csv")
+    # first create the table!
+
+    f = open("books.csv")
     reader = csv.reader(f)
-    for origin, destination, duration in reader:
-        db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
-                    {"origin": origin, "destination": destination, "duration": duration})
-        print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO books (isbn, title, author, year) "
+                   "VALUES (:isbn, :title, :author, :year)",
+                   {"isbn": isbn, "title": title, "author": author, "year": year})
     db.commit()
+
 
 if __name__ == "__main__":
     main()
