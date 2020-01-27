d9a834fffa04d64bdd3b05565ba6fffcc43fccd2
Isabelle <ibernard@necando.com>
2020-01-27 15:22:43 -0500

    un peu perdue à voir demain

--- project1_books_app/application.py
+++ project1_books_app/application.py
@@ -22,6 +22,7 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+# if table books doesn't exist, create it
 if not engine.dialect.has_table(engine, "books"):
     db.execute('CREATE TABLE "books" ('
                'isbn Integer NOT NULL,'
@@ -30,6 +31,7 @@ if not engine.dialect.has_table(engine, "books"):
                'year INTEGER NOT NULL,'
                'PRIMARY KEY (isbn));')
     db.commit()
+# if table users doesn't exist, crate it
 if not engine.dialect.has_table(engine, "users"):
     db.execute('CREATE TABLE "users" ('
                'username VARCHAR PRIMARY KEY,'
@@ -43,6 +45,36 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    username = request.form.get("new_username")
+    password = request.form.get("new_password")
+    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+               {"username": username, "password": password})
+    db.commit()
+    return render_template("register.html")
+
+
+@app.route("/thanks", methods=["GET", "POST"])
+def thanks():
+    if request.method == "GET":
+        return render_template("error.html", message="Please register or login first")
+
+    username = request.form.get("new_username")
+    password = request.form.get("new_password")
+    user = db.execute("SELECT username from users "
+                      "WHERE username=:username",
+                      {"username": username}).fetchone()
+    if user is not None:
+        return render_template("error.html", message="User already taken")
+    else:
+        username = request.form.get("username")
+        db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+                   {"username": username, "password": password})
+        db.commit()
+        return render_template("thanks.html", username=username)
+
+
 @app.route("/welcome", methods=["GET", "POST"])
 def welcome():
     if request.method == "GET":
@@ -54,31 +86,6 @@ def welcome():
                   {"username": username, "password": password}).rowcount == 0:
         return render_template("error.html", message="Invalid login info.")
     return render_template("welcome.html", username=username)
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    username = request.form.get("new_username")
-    password = request.form.get("new_password")
-    user = db.execute("SELECT username from users "
-                      "WHERE username=:username",
-                      {"username": username}).fetchone()
-    db.commit()
-    if user is not None:
-        return render_template("error.html", message="User already taken")
-    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-               {"username": username, "password": password})
-    db.commit()
-    return render_template("register.html", username=username)
-
-
-@app.route("/thanks", methods=["GET", "POST"])
-def thanks():
-    if request.method == "GET":
-        return render_template("error.html", message="Please register or login first")
-    else:
-        username = request.form.get("username")
-        return render_template("thanks.html", username=username)
 
 
 if __name__ == '__main__':
