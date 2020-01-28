94898c673e90b402e2d8796d5bc12ef6475b13e5
Isabelle <ibernard@necando.com>
2020-01-28 15:30:49 -0500

    added explanation on what the api does

--- project1_books_app/application.py
+++ project1_books_app/application.py
@@ -89,3 +89,11 @@ def welcome():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+"""
+Once they log in, they will be able to search for books, leave reviews for individual books, 
+and see the reviews made by other people. You’ll also use the a third-party API by Goodreads, 
+another book review website, to pull in ratings from a broader audience. 
+Finally, users will be able to query for book details and book reviews programmatically via your 
+website’s API.
+"""
