7213fabfa3dc8aad814ad50da653d16566bf766f
Isabelle <ibernard@necando.com>
2020-01-30 14:17:39 -0500

    updated to do in application.py

--- project1_books_app/application.py
+++ project1_books_app/application.py
@@ -120,40 +120,10 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 """
-Once they log in, they will be able to search for books, leave reviews for individual books, 
-and see the reviews made by other people. You’ll also use the a third-party API by Goodreads, 
-another book review website, to pull in ratings from a broader audience. 
-Finally, users will be able to query for book details and book reviews programmatically via your 
-website’s API.
-
-- OK Registration: Users should be able to register for your website, providing (at minimum) 
-a username and password.
-
-- OK Login: Users, once registered, should be able to log in to your website with their 
-username and password.
-
-- OK (copié d'un exemple): Logout: Logged in users should be able to log out of the site.
-Hint: In terms of how to “log a user in,” recall that you can store information inside of the 
-session, which can store different values for different users. In particular, if each user has 
-an id, then you could store that id in the session (e.g., in session["user_id"]) to keep track 
-of which user is currently logged in.
-
-- OK Import: Provided for you in this project is a file called books.csv, which is a spreadsheet 
-in CSV format of 5000 different books. Each one has an ISBN number, a title, an author, and a 
-publication year. In a Python file called import.py separate from your web application, 
-write a program that will take the books and import them into your PostgreSQL database. 
-You will first need to decide what table(s) to create, what columns those tables should have, 
-and how they should relate to one another. Run this program by running python3 import.py to 
-import the books into your database, and submit this program with the rest of your project code.
-
 TO DO:
 - One table for reviews
 
-- Search: Once a user has logged in, they should be taken to a page where they can 
-search for a book. Users should be able to type in the ISBN number of a book, the title of a book,
- or the author of a book. After performing the search, your website should display a list of 
- possible matching results, or some sort of message if there were no matches. If the user typed
- in only part of a title, ISBN, or author name, your search page should find matches for those as well!
+- Search: If the user typed  in only part of a title, ISBN, or author name, your search page should find matches for those as well!
  
 - Book Page: When users click on a book from the results of the search page, they should 
 be taken to a book page, with details about the book: its title, author, publication year, 
@@ -180,12 +150,6 @@ follow the format:
     "average_score": 5.0
 }
 If the requested ISBN number isn’t in your database, your website should return a 404 error.
-
-- You should be using raw SQL commands (as via SQLAlchemy’s execute method) in order to make 
-database queries. You should not use the SQLAlchemy ORM (if familiar with it) for this project.
-
-- In README.md, include a short writeup describing your project, what’s contained in each file, 
-and (optionally) any other additional information the staff should know about your project.
 
 - If you’ve added any Python packages that need to be installed in order to run your web 
 application, be sure to add them to requirements.txt!
